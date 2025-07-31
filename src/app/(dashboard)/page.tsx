26b16d1ef856d68b984ad41ac9f4fbcb4f0d7a8f
Addis <addis.ugr-4143-15@aau.edu.et>
2025-07-31 17:11:59 +0300

    feat: Add dashboard-sidebar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
-import { HomeView } from "./modules/home/ui/views/home-view";
+import { HomeView } from "../modules/home/ui/views/home-view";
 
 export default async function Home() {
   const session = await auth.api.getSession({
