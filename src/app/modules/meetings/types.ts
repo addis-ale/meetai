365cf0d16727e61723b983703d9a38fcd014000b
Addis <addis.ugr-4143-15@aau.edu.et>
2025-08-12 11:17:16 +0300

    feat: Add meetings-data-table

--- src/app/modules/meetings/types.ts
+++ src/app/modules/meetings/types.ts
@@ -1,3 +1,5 @@
 import { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/trpc/routers/_app";
+export type MeetingGetMany =
+  inferRouterOutputs<AppRouter>["meetings"]["getMany"]["item"];
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
