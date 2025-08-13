7110308e60b133aab2e157f4cd0058c8a3a57b4f
Addis <addis.ugr-4143-15@aau.edu.et>
2025-08-13 23:10:26 +0300

    feat: add video call

---
+++ src/app/call/[meetingId]/page.tsx
@@ -0,0 +1,40 @@
+import { Suspense } from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { CallView } from "@/app/modules/call/ui/views/call-view";
+
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>;
+}
+const Page = async ({ params }: Props) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("sign-in");
+  }
+  const { meetingId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({
+      id: meetingId,
+    })
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>todo</p>}>
+        <ErrorBoundary fallback={<p>todo</p>}>
+          <CallView meetingId={meetingId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
--- src/app/modules/meetings/server/procedures.ts
+++ src/app/modules/meetings/server/procedures.ts
@@ -12,7 +12,30 @@ import {
 } from "@/constants";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { MeetingStatus } from "../types";
+import { streamVideo } from "@/lib/stream-video";
+import { generateAvatarUri } from "@/lib/avatar";
 export const meetingsRouter = createTRPCRouter({
+  generateToken: protectedProcedure.mutation(async ({ ctx }) => {
+    await streamVideo.upsertUsers([
+      {
+        id: ctx.auth.user.id,
+        name: ctx.auth.user.name,
+        role: "admin",
+        image:
+          ctx.auth.user.image ??
+          generateAvatarUri({ seed: ctx.auth.user.name, variant: "initials" }),
+      },
+    ]);
+    //create a token
+    const expirationTime = Math.floor(Date.now() / 1000) + 3600; //1 hr
+    const issuedAt = Math.floor(Date.now() / 1000) - 60;
+    const token = streamVideo.generateUserToken({
+      user_id: ctx.auth.user.id,
+      exp: expirationTime,
+      validity_in_seconds: issuedAt,
+    });
+    return token;
+  }),
   remove: protectedProcedure
     .input(
       z.object({
@@ -149,6 +172,48 @@ export const meetingsRouter = createTRPCRouter({
         })
         .returning();
       //TODO: Create Stream Call, Upsert stream users
+      const call = streamVideo.video.call("default", createdMeeting.id);
+      await call.create({
+        data: {
+          created_by_id: ctx.auth.user.id,
+          custom: {
+            meetingId: createdMeeting.id,
+            meetingName: createdMeeting.name,
+          },
+          settings_override: {
+            transcription: {
+              language: "en",
+              mode: "auto-on",
+              closed_caption_mode: "auto-on",
+            },
+            recording: {
+              mode: "auto-on",
+              quality: "1080p",
+            },
+          },
+        },
+      });
+      const [existngAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, createdMeeting.agentId));
+      if (!existngAgent) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Agent not found",
+        });
+      }
+      await streamVideo.upsertUsers([
+        {
+          id: existngAgent.id,
+          name: existngAgent.name,
+          role: "user",
+          image: generateAvatarUri({
+            seed: existngAgent.name,
+            variant: "bottsNeutral",
+          }),
+        },
+      ]);
       return createdMeeting;
     }),
 });
